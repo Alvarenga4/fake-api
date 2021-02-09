808715b083429d1ff10e9c7c925dbd06f514b392
Francisco Carlos <franciscocarlos002@outlook.com>
2021-02-09 16:37:14 -0300

    refactor: change json data

--- src/controllers/companyContractController.js
+++ src/controllers/companyContractController.js
@@ -1,18 +1,8 @@
+const company = require('../json/company.json');
+
 module.exports = {
   async show(req, res) {
     try {
-      const { providerslug, contractcode } = req.headers;
-
-      if (!providerslug || !contractcode) return res.status(404).json({ error: 'providerSlug or contractCode is empty' });
-
-      const company = {
-        "companyId": 1,
-        "compayName": "Empresa do seu zé",
-        "companyGroupId": 22,
-        "over": 4,
-        "tax": 6,
-        "taxInstallment": 11
-      };
 
       return res.status(200).json(company);
 
